da7d0b0aefa1bf9c25f6344dcb9ddea3683dcdbc
GRONDIN Clarel <clarelgrondin@gmail.com>
2024-10-25 10:41:19 +0400

    login add commit

--- security/login.php
+++ security/login.php
@@ -11,12 +11,94 @@
 <!-- Page faite par Clarel-->
 <body>
 
-<form action=""></form>
+<?php
+$db_username = 'root';
+$db_password = '';
+$conn = new PDO( 'mysql:host=localhost;dbname=projet_dg', $db_username, $db_password );
+if(!$conn){
+    die("Fatal Error: Connection Failed!");
+}
+
+?>
+
+<section class="h-100 gradient-form" style="background-color: #eee;">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-xl-10">
+                <div class="card rounded-3 text-black">
+                    <div class="row g-0">
+                        <div class="col-lg-6">
+                            <div class="card-body p-md-5 mx-md-4">
+
+                                <div class="text-center">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                                         style="width: 185px;" alt="logo">
+                                    <h4 class="mt-1 mb-5 pb-1">Nous sommes Projet_dg</h4>
+                                </div>
+
+                                <form method="post">
+                                    <p>Veuillez vous connecter à votre compte :</p>
+
+                                    <div data-mdb-input-init class="form-outline mb-4">
+                                        <input type="text" id="pseudo" name="pseudo" class="form-control"
+                                               placeholder="Pseudo" />
+<!--                                        <label class="form-label" for="form2Example11">Pseudo</label>-->
+                                    </div>
+
+                                    <div data-mdb-input-init class="form-outline mb-4">
+                                        <input type="password" id="pass" name="pass" class="form-control"
+                                        placeholder="Mot de Passe"/>
+<!--                                        <label class="form-label" for="form2Example22">Mot de passe</label>-->
+                                    </div>
+
+                                    <div class="text-center pt-1 mb-5 pb-1">
+                                        <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">
+                                            Connexion
+                                        </button>
+                                        <a class="text-muted" href="#!">Mot de passe oublié?</a>
+                                    </div>
+
+                                    <div class="d-flex align-items-center justify-content-center pb-4">
+                                        <p class="mb-0 me-2">Pas encore inscrit(e)?</p>
+                                        <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-danger">S'inscrire</button>
+                                    </div>
+
+                                </form>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                            <div class="text-black px-3 py-4 p-md-5 mx-md-4">
+                                <h4 class="mb-4">Nous vendons plus que des voitures</h4>
+                                <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 </body>
 
 <!-- Scripts -->
+
+<?php
+$stmt = $conn->prepare("SELECT * FROM users WHERE pseudo = ?");
+$stmt->execute([$_POST['pseudo']]);
+$user = $stmt->fetch();
+
+if ($user && password_verify($_POST['pass'], $user['pass']))
+{
+    echo "valid!";
+} else {
+    echo "invalid";
+}
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-
 </html>
