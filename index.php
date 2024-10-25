9aafde27d7ab44b5446e35d8c21ee3ff379340f1
Mat911 <matisse.demontis@gmail.com>
2024-10-25 10:03:38 +0400

    modification de la nav bar et ajour de la barre de recherche | image exemple

--- index.php
+++ index.php
@@ -19,80 +19,44 @@
     </head>
 
     <body>
-        <!-- Nav barre avec une icone a gauche au milieux 3 bouton pour naviger sur differentes pages et a droite 2 bouton login et sign in -->
-         <!doctype html>
-         <html lang="en">
-             <head>
-                 <title>Title</title>
-                 <!-- Required meta tags -->
-                 <meta charset="utf-8" />
-                 <meta
-                     name="viewport"
-                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
-                 />
-         
-                 <!-- Bootstrap CSS v5.2.1 -->
-                 <link
-                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-                     rel="stylesheet"
-                     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-                     crossorigin="anonymous"
-                 />
-             </head>
-         
-             <body>
-                 <!-- Nav bar Exemple -->
-                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                     <div class="container-fluid">
-                         <a class="navbar-brand" href="#">
-                             <img src="path/to/icon.png" alt="Icon" width="30" height="24">
-                         </a>
-                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                             <span class="navbar-toggler-icon"></span>
-                         </button>
-                         <div class="collapse navbar-collapse" id="navbarNav">
-                             <ul class="navbar-nav mx-auto">
-                                 <li class="nav-item">
-                                     <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                 </li>
-                                 <li class="nav-item">
-                                     <a class="nav-link" href="#">Features</a>
-                                 </li>
-                                 <li class="nav-item">
-                                     <a class="nav-link" href="#">Pricing</a>
-                                 </li>
-                             </ul>
-                             <div class="d-flex">
-                                 <button class="btn btn-outline-success me-2" type="button">Login</button>
-                                 <button class="btn btn-outline-primary" type="button">Sign In</button>
-                             </div>
-                         </div>
-                     </div>
-                 </nav>
-         
+        <!-- Nav bar avec icon|menu|login -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="path/to/icon.png" alt="Icon" width="30" height="24">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex">
+                        <button class="btn btn-outline-success me-2" type="button">Login</button>
+                        <button class="btn btn-outline-primary" type="button">Sign In</button>
+                    </div>
+                </div>
+            </div>
+        </nav> 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <!-- Recherche -->
+        <div class="container my-4">
+            <form method="GET" action="">
+                <div class="input-group mb-3">
+                    <input type="text" name="search" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" >
+                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                </div>
+            </form>
+        </div>
 
         <!-- Bootstrap JavaScript Libraries -->
         <script
